166c6f667471ff2d402e0cbf95c8b61cb1b5b909
Jason E Bailey <jeb@apache.org>
2018-05-30 14:31:29 -0400

    SLING-7699 add ResourceStream and unit tests
    updates java version to jdk8

---
+++ src/test/java/org/apache/sling/api/resource/ResourceStreamTest.java
@@ -0,0 +1,95 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sling.api.resource;
+
+import static org.junit.Assert.assertEquals;
+
+import java.text.ParseException;
+
+import org.apache.sling.testing.mock.sling.junit.SlingContext;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class ResourceStreamTest {
+
+    @Rule
+    public final SlingContext context = new SlingContext();
+
+    private static final String PATH = "/content/sample/en";
+
+    @Before
+    public void setUp() throws ParseException {
+        context.load().json("/data.json", PATH);
+    }
+
+    @Test
+    public void testObtainResourceFromContext() {
+        Resource resource = context.resourceResolver().getResource(PATH);
+        assertEquals("en", resource.getName());
+    }
+
+    @Test
+    public void testResourceStream() {
+        Resource resource = context.resourceResolver().getResource(PATH);
+        Object[] found = ResourceStream.from(resource).stream().toArray();
+        assertEquals(10, found.length);
+    }
+
+    @Test
+    public void testResourceStreamBranchSelector() {
+        Resource resource = context.resourceResolver().getResource(PATH);
+        Object[] found = ResourceStream.from(resource)
+                .setBranchSelector(r -> r.getValueMap().get("jcr:primaryType", "").equals("app:Page")).stream()
+                .toArray();
+        assertEquals(3, found.length);
+    }
+
+    @Test
+    public void testResourceStreamResourceSelector() {
+        Resource resource = context.resourceResolver().getResource(PATH);
+        Object[] found = ResourceStream.from(resource)
+                .setResourceSelector(r -> r.getValueMap().get("jcr:primaryType").equals("app:PageContent")).stream()
+                .toArray();
+        assertEquals(4, found.length);
+    }
+
+    @Test
+    public void testResourceStreamLowLimit() {
+        Resource resource = context.resourceResolver().getResource(PATH);
+        Object[] found = ResourceStream.from(resource)
+                .setResourceSelector(r -> r.getValueMap().get("jcr:primaryType").equals("app:PageContent")).limit(3)
+                .stream().toArray();
+        assertEquals(3, found.length);
+    }
+
+    @Test
+    public void testResourceStreamHighLimit() {
+        Resource resource = context.resourceResolver().getResource(PATH);
+        Object[] found = ResourceStream.from(resource)
+                .setResourceSelector(r -> r.getValueMap().get("jcr:primaryType").equals("app:PageContent")).limit(7)
+                .stream().toArray();
+        assertEquals(4, found.length);
+    }
+
+    @Test
+    public void testResourceStreamRange() {
+        Resource resource = context.resourceResolver().getResource(PATH);
+        Object[] found = ResourceStream.from(resource)
+                .setResourceSelector(r -> r.getValueMap().get("jcr:primaryType").equals("app:PageContent")).range(2, 4)
+                .stream().toArray();
+        assertEquals(3, found.length);
+    }
+
+}
