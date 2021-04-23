eeb8f84f92a2f1bbfad236ba9dca9c15c2e837f3
Jörg Hoh <joerghoh@users.noreply.github.com>
2021-04-23 14:10:55 +0200

    SLING-8759 use computeIfAbsent to reduce code size (#26)

--- src/main/java/org/apache/sling/api/adapter/SlingAdaptable.java
+++ src/main/java/org/apache/sling/api/adapter/SlingAdaptable.java
@@ -94,22 +94,15 @@ public abstract class SlingAdaptable implements Adaptable {
      */
     @SuppressWarnings("unchecked")
     public <AdapterType> AdapterType adaptTo(Class<AdapterType> type) {
-        AdapterType result = null;
         synchronized ( this ) {
-            if ( adaptersCache != null ) {
-                result = (AdapterType) adaptersCache.get(type);
+            if (ADAPTER_MANAGER == null) {
+                return null;
             }
-            if ( result == null ) {
-                final AdapterManager mgr = ADAPTER_MANAGER;
-                result = (mgr == null ? null : mgr.getAdapter(this, type));
-                if ( result != null ) {
-                    if ( adaptersCache == null ) {
-                        adaptersCache = new HashMap<Class<?>, Object>();
-                    }
-                    adaptersCache.put(type, result);
-                }
+            if (adaptersCache == null) {
+                adaptersCache = new HashMap<Class<?>, Object>();
             }
+            return (AdapterType) adaptersCache.computeIfAbsent(type,
+                    klazz -> ADAPTER_MANAGER.getAdapter(this, type));
         }
-        return result;
     }
 }
