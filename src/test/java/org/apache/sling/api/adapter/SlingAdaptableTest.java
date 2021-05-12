4ea29bee1688f73086adfebceadad02064ec8f2a
Julian Sedding <jsedding@apache.org>
2021-05-12 15:59:36 +0200

    SLING-10371 - SlingAdaptable: ConcurrentModificationException on nested adaptTo() calls
    - test concurrent cache initialization

--- src/test/java/org/apache/sling/api/adapter/SlingAdaptableTest.java
+++ src/test/java/org/apache/sling/api/adapter/SlingAdaptableTest.java
@@ -24,10 +24,23 @@ import static org.junit.Assert.assertNull;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.*;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.mockito.stubbing.Answer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class SlingAdaptableTest {
 
@@ -65,8 +78,8 @@ public class SlingAdaptableTest {
     }
     
     // SLING-10371
-    @Test()
-    public void testNestedAdaptTo() {
+    @Test
+    public void testNestedAdaptTo() throws ExecutionException, InterruptedException {
         SlingAdaptable.setAdapterManager(adapterMgr);
         SuperTypeCallingAdaptable adaptable = new SuperTypeCallingAdaptable();
         when(adapterMgr.getAdapter(eq(adaptable),eq(TestAdapterType.class)))
@@ -79,11 +92,21 @@ public class SlingAdaptableTest {
         .thenAnswer(invocation -> {
             return "someValue";
         });
-        assertNotNull(adaptable.adaptTo(TestAdapterType.class));
+        final List<TestAdapterType> results = runConcurrently(64, 1, () -> adaptable.adaptTo(TestAdapterType.class));
+        assertAllTheSame(adaptable.adaptTo(TestAdapterType.class), results);
     }
-    
 
-    public class SuperTypeCallingAdaptable extends SlingAdaptable {
+    @Test
+    public void testConcurrentInitializationOfAdaptersCache() throws InterruptedException, ExecutionException {
+        SlingAdaptable.setAdapterManager(adapterMgr);
+        when (adapterMgr.getAdapter(any(), eq(TestAdapterType.class))).then((Answer<TestAdapterType>) invocationOnMock -> new TestAdapterType());
+
+        List<TestAdapterType> results = runConcurrently(64, 1, () -> sut.adaptTo(TestAdapterType.class));
+        verify(adapterMgr, times(1)).getAdapter(any(), eq(TestAdapterType.class));
+        assertAllTheSame(sut.adaptTo(TestAdapterType.class), results);
+    }
+
+    public static class SuperTypeCallingAdaptable extends SlingAdaptable {
         @Override
         public <AdapterType> @Nullable AdapterType adaptTo(@NotNull Class<AdapterType> type) {
             // always fallback to the supertype implementation
@@ -98,8 +121,41 @@ public class SlingAdaptableTest {
     }
 
     // test adaptables
-    public class TestAdapterType {}
+    public static class TestAdapterType {}
 
     // pseudo adaptable
-    public class TestAdapterType2 {}
+    public static class TestAdapterType2 {}
+
+    private static <T> void assertAllTheSame(T referenceObject, Iterable<T> results) {
+        assertNotNull("reference object should not be null", referenceObject);
+        results.forEach(result -> assertSame("cached adaptable objects should be the same instance", referenceObject, result));
+    }
+
+    private static <T> List<T> runConcurrently(int threads, int timeoutSeconds, Callable<T> callable) throws InterruptedException, ExecutionException {
+        final CountDownLatch countDownLatch = new CountDownLatch(threads);
+        final ExecutorService executorService = Executors.newFixedThreadPool(threads);
+        final List<Future<T>> results = new ArrayList<>(threads);
+
+        for (int i = 0; i < threads; i++) {
+            results.add(executorService.submit(() -> {
+                countDownLatch.countDown();
+                try {
+                    countDownLatch.await();
+                } catch (InterruptedException e) {
+                    throw new IllegalStateException("Should never be interrupted", e);
+                }
+                return callable.call();
+            }));
+        }
+        executorService.shutdown();
+        if (executorService.awaitTermination(timeoutSeconds, TimeUnit.SECONDS)) {
+            List<T> resultList = new ArrayList<>();
+            for (Future<T> result : results) {
+                resultList.add(result.get());
+            }
+            return resultList;
+        }
+        fail("Concurrent run did not finish within " + timeoutSeconds + " seconds");
+        throw new IllegalStateException("unreachable due to \"fail\" - just to satisfy the compiler");
+    }
 }
