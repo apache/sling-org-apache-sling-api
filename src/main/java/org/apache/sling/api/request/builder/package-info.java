42736ebbe0f37376ff0aecc0498b50923a964762
Eric Norman <enorman@apache.org>
2022-08-15 11:46:28 -0700

    SLING-11531 add RequestParameter factory to Builders (#45)

--- src/main/java/org/apache/sling/api/request/builder/package-info.java
+++ src/main/java/org/apache/sling/api/request/builder/package-info.java
@@ -16,5 +16,5 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-@org.osgi.annotation.versioning.Version("1.1")
+@org.osgi.annotation.versioning.Version("1.2")
 package org.apache.sling.api.request.builder;
