9b872a1c3fd17d75b4e573fbc30a4fc9fae166ca
Jörg Hoh <joerghoh@users.noreply.github.com>
2024-05-30 11:28:55 +0200

    SLING-12062 add LazyBindings.putOnly (#52)
    * SLING-12062 add LazyBindings.putOnly
    * add test coverage for the Sling Bindings

--- src/main/java/org/apache/sling/api/scripting/SlingBindings.java
+++ src/main/java/org/apache/sling/api/scripting/SlingBindings.java
@@ -182,7 +182,7 @@ public class SlingBindings extends LazyBindings {
      */
     protected void safePut(final String key, final Object value) {
         if ( value != null ) {
-            this.put(key, value);
+            this.putOnly(key, value);
         }
     }
 
@@ -191,7 +191,7 @@ public class SlingBindings extends LazyBindings {
      * @param flush Whether to flush or not
      */
     public void setFlush(boolean flush) {
-        put(FLUSH, flush);
+        putOnly(FLUSH, flush);
     }
 
     /**
